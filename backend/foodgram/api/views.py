e4e5d72dd85af0d15e609268ae74832f8083dedd
Sabina <sabina_045@mail.ru>
2023-06-20 12:59:30 +0700

    Исправления ревью ко 2 этапу спринта

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
